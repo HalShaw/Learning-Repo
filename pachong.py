32fbdb542ceeaad0870039a3e0bf1c1c86b38eaa
unknown <xiaohaogreat@qq.com>
2016-06-09 13:31:45 +0800

    6-9 1:31

--- pachong.py
+++ pachong.py
@@ -28,6 +28,7 @@ class Article(object):
 		content=re.findall(r'<p>(.*?)</p>',article,re.S)
 		img=re.search(r'<img alt="(.*?)" src="(.*?!article\.foil)"',article,re.M|re.I)
 		comment_count=re.findall(r'<span class="wscn-cm-counter">(.*?)</span>',article,re.S)
+
 		if img==None and comment_count[0]==None:#有可能文章没有图片和评论，考虑以下几种情况
 			return str(title[0]),str(author.group(2)),datetime.strptime(year+'-'+month+'-'+day+' '+hour,'%Y-%m-%d %H:%M:%S'),''.join(content[:-4]),None,0
 		elif img!=None and comment_count[0]==None:
@@ -40,29 +41,35 @@ class Article(object):
 	def save_content(self,title,author,post_at,content,img,comment_count):
 		#连接并保存到数据库
 		conn = sqlite3.connect('article.db')
+		conn.execute("drop table art")#如果存在一个art表，删除，新建一个art表
+		conn.execute("CREATE TABLE art(title varchar(80) PRIMARY KEY not null, author varchar(10),post_at TEXT not null,content varchar(255) not null,img varchar(20) ,comment_count integer);")
 		conn.execute("INSERT INTO art (title,author,post_at,content,img,comment_count)values(?,?,?,?,?,?)",(title,author,post_at,content,img,comment_count))
 		result=conn.execute("SELECT* FROM art")
 		return list(result)
 		conn.close()
 
 	def spider(self):
-		#for i in range(19999,20002):
-		for i in random.sample(range(19999,20002),1):
+		cou=1
+		#for i in range(1999,20002):#顺序生成URL
+		for i in random.sample(range(1999,20016),500):#随机生成url
 			try:
 				full_url=self.url+'node'+'/'+str(i)#URL格式
 				page = urllib.request.urlopen(full_url,timeout=10)#设置请求时间限制
 				pages= page.read().decode('utf-8','ignore')
 				lst=self.get_content(pages)
-				self.save_content(lst[0],lst[1],lst[2],lst[3],lst[4],lst[5])#不打印出结果
-				#print(self.save_content(lst[0],lst[1],lst[2],lst[3],lst[4],lst[5]))#打印出结果
+				#self.save_content(lst[0],lst[1],lst[2],lst[3],lst[4],lst[5])#不打印出结果
+				print(self.save_content(lst[0],lst[1],lst[2],lst[3],lst[4],lst[5]))#打印出结果
 				print('Successfully Downloaded...\n')
+				cou+=1
 				time.sleep(2)
+				if cou>300:
+					sys.exit()
+				else:
+					continue
 			except urllib.error.URLError as e:
 				print(e)
-				continue
-				sys.exit()
-
-
+				
+				
 	def main(self):
 		#多线程和多进程爬取
 		#my_thread = threading.Thread(target = self.spider)#多线程
@@ -70,101 +77,8 @@ class Article(object):
 		my_thread.start()
 		my_thread.join()
 
-'''
-def get_content(article):
-		title = re.findall(r'<h1 class="article-title">(.*?)</h1>',article,re.S)
-		author=re.search(r'<span class="item author">(.*?)target="_blank">(.*?)</a>(.*?)</span>',article,re.S)
-		post_at=re.findall(r'<span class="item time">(.*?)</span>',article,re.S)
-		time=post_at[0]
-		year=time[:4]
-		month=time[5:7]
-		day=time[8:10]
-		hour=time[12:]
-		content=re.findall(r'<p>(.*?)</p>',article,re.S)
-		img=re.search(r'<img alt="(.*?)" src="(.*?!article\.foil)"',article,re.M|re.I)
-		comment_count=re.findall(r'<span class="wscn-cm-counter">(.*?)</span>',article,re.S)
-		if img==None and comment_count[0]==None:
-			return str(title[0]),str(author.group(2)),datetime.strptime(year+'-'+month+'-'+day+' '+hour,'%Y-%m-%d %H:%M:%S'),''.join(content[:-4]),None,0
-		elif img!=None and comment_count[0]==None:
-			return str(title[0]),str(author.group(2)),datetime.strptime(year+'-'+month+'-'+day+' '+hour,'%Y-%m-%d %H:%M:%S'),''.join(content[:-4]),str(img.group(2)),0
-		elif img==None and comment_count[0]!=None:
-			return str(title[0]),str(author.group(2)),datetime.strptime(year+'-'+month+'-'+day+' '+hour,'%Y-%m-%d %H:%M:%S'),''.join(content[:-4]),None,int(comment_count[0])
-		else:
-			return str(title[0]),str(author.group(2)),datetime.strptime(year+'-'+month+'-'+day+' '+hour,'%Y-%m-%d %H:%M:%S'),''.join(content[:-4]),str(img.group(2)),int(comment_count[0])
-def save_content(title,author,post_at,content,img,comment_count):
-		conn = sqlite3.connect('article.db')
-		conn.execute("INSERT INTO art (title,author,post_at,content,img,comment_count)values(?,?,?,?,?,?)",(title,author,post_at,content,img,comment_count))
-		result=conn.execute("SELECT count(*) FROM art")
-		cur = conn.cursor()
-		count=cur.fetchall()
-		#c=count[0]
-		print(list(result))
-		#return list(result)
-		conn.close()
-
-def get_title(article1):
-	title = re.findall(r'<h1 class="article-title">(.*?)</h1>',article1,re.S)
-	return str(title[0])
-
-def get_author(article2):
-	author=re.search(r'<span class="item author">(.*?)target="_blank">(.*?)</a>(.*?)</span>',article2,re.S)
-	return str(author.group(2))
-
-def get_time(article3):
-	post_at=re.findall(r'<span class="item time">(.*?)</span>',article3,re.S)
-	time=post_at[0]
-	year=time[:4]
-	month=time[5:7]
-	day=time[8:10]
-	hour=time[12:]
-	return datetime.strptime(year+'-'+month+'-'+day+' '+hour,'%Y-%m-%d %H:%M:%S')
-
-def get_content(article4):
-	content=re.findall(r'<p>(.*?)</p>',article4,re.S)
-	return ''.join(content[:-4])
-
-def get_img(article5):
-	img=re.search(r'<img alt="(.*?)" src="(.*?!article\.foil)"',article5,re.M|re.I)
-	if img==None:
-		return None
-	else:
-		return str(img.group(2))
-
-def get_comment(article6):
-	comment_count=re.findall(r'<span class="wscn-cm-counter">(.*?)</span>',article6,re.S)
-	if comment_count[0]==None:
-		return 0
-	else:
-		return int(comment_count[0])
-
-def spider():
-	for i in range(1999,2015):
-		try:
-			timeout = 20
-			socket.setdefaulttimeout(timeout)
-			url = "http://wallstreetcn.com/"
-			full_url=url+'node'+'/'+str(i)
-			page = urllib.request.urlopen(full_url)
-			pages= page.read().decode('utf-8','ignore')
-			lst=get_content(pages)
-			save_content(lst[0],lst[1],lst[2],lst[3],lst[4],lst[5])
-			#print(save_content(get_title(pages),get_author(pages),get_time(pages),get_content(pages),get_img(pages),get_comment(pages)))
-			print('Successfully Downloaded...\n')
-			time.sleep(1)
-			#return get_title(pages),get_author(pages),get_time(pages),get_content(pages),get_img(pages),get_comment(pages)
-		except urllib.error.URLError as e:
-			print(e)
-			continue
-
-def main():
-	#my_thread = threading.Thread(target = spider)
-	my_thread = multiprocessing.Process(target = spider)
-	my_thread.start()
-	my_thread.join()
-	'''			
-
 if __name__ == '__main__':
-	a=Article()
+	a=Article()#实例化
 	a.main()
 
 
